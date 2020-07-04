23b2f192abcd949d908c93b56d42d0d3dd06a875
WJM99-JMOK <67691821+WJM99-JMOK@users.noreply.github.com>
2020-07-04 21:12:19 +0800

    Add files via upload

--- JAVA8基础应用/Ex2_1.java
+++ JAVA8基础应用/Ex2_1.java
@@ -1,33 +1,17 @@
-import java.util.Scanner;
-
 public class Ex2_1 {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-//		   Scanner scan = new Scanner(System.in);
-//	        System.out.println("请输入割圆次数：");
-//	        int n = scan.nextInt();
-//	        cut(n);
-//	}
-// 
-//	private static void cut(int n) {
-//		double y = 1.0;
-//		for(int i = 0; i <= n; i++) {
-//			double π = 3 * Math.pow(2, i) * y;
-//			System.out.println()
-//		}
 		double sum = 1 ; 
-		for(int a = 3; a <=99; a = a + 2) {
-			if((a/2)%2 == 1) {
-			sum = sum - 1.0/a;
-			
-			}else{
-			sum = sum +1.0/a;	
-			}
-			
-		}
-		double pai = 4 * sum;
 		
+		// a为分母
+		for (int a = 3; a <= 99; a = a + 2) {
+			if ((a / 2) % 2 == 1) {
+				sum = sum - 1.0 / a;
+			} else {
+				sum = sum + 1.0 / a;	
+			}	
+		}
+		double pai = 4 * sum;	
 		System.out.println(pai);
 	}
 }
