cbe1cc7bdfbc6d1f6cd25c0d6de989516b2a71f6
WJM99-JMOK <67691821+WJM99-JMOK@users.noreply.github.com>
2020-07-14 21:14:24 +0800

    Add files via upload

--- スッキリわかるjava入門/Cleric.java
+++ スッキリわかるjava入門/Cleric.java
@@ -1,33 +1,32 @@
-package j8;
-
-import java.util.Random;
+package j9;
 
 public class Cleric {
 	public String name;
 	public int hp;
-	public final int maxHp = 50;
+	static final int maxHp = 50;
 	public int mp;
-	public final int maxMp = 10;
+	static final int maxMp = 10;
+
 	
-	public void selfAid() {
-		this.hp = this.maxHp;
-		this.mp -= 5;
+	Cleric(String name,int hp,int mp){
+		this.name = name;
+		this.hp = hp;
+		this.mp = mp;			
 	}
 	
-	public int pray(int jikan) {
-		System.out.println(jikan + "秒の時間をかかった。");
-		int kaifukujikan = new Random().nextInt(3) + jikan;	
-		int kaifusu = Math.min(this.maxMp - this.mp,kaifukujikan);
-		this.mp += kaifusu; 
-		System.out.println(this.mp + "点回復した。");
-		return kaifukujikan;
+	Cleric(String name,int hp){
+		this.name = name;
+		this.hp = hp;
+		this.mp = Cleric.maxMp;	
 	}
 	
-	public static void main(String[] args) {
-		Cleric a = new Cleric();
-		a.name = "wjm";
-		a.hp = 40;
-		a.mp = 0;
-		a.pray(2);
+	Cleric(String name){
+		this.name = name;
+		this.hp = Cleric.maxHp;
+		this.mp = Cleric.maxMp;	
 	}
+	
+	Cleric(){
+	}
+	
 }
