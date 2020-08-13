95fc6ea7c136155024b2b6b05932fd9a83e53fde
WJM99-JMOK <67691821+WJM99-JMOK@users.noreply.github.com>
2020-08-13 22:37:02 +0800

    Add files via upload

--- スッキリわかるjava入門/Ex5_3.java
+++ スッキリわかるjava入門/Ex5_3.java
@@ -8,15 +8,13 @@ public class Ex5_3 {
 		email(address,text);
 	}
 	
-	public static void email(String title,String address,String text){
-		System.out.println(title + "に、以下のメールを送信しました。");
-		System.out.println(address + "メールのタイトル");
-		System.out.println(text + "メールの本文");
+	public static void email(String address,String title,String text){
+		System.out.println(address + "に、以下のメールを送信しました。");
+		System.out.println(title != null ?"件名："+ title : "件名：無題" );
+		System.out.println("メールの本文" + text);
 	}
 	
 	public static void email(String address,String text){
-		System.out.println(address + "に、以下のメールを送信しました。");
-		System.out.println("件名：無題");
-		System.out.println("メールの本文" + text);
+		email(address,null,text);
 	}
 }
